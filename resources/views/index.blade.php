adc685d83a772c7de8690b0b019555dbc4cb616d
GewoonGianni <giannimeesters@gmail.com>
2020-05-26 22:21:10 +0200

    add line of text on index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div class="col-12 text-center">
         <h1 class="display-3 text-center my-5">Dit is een blog website!</h1>
+        <p class="text-muted">En deze site voldet aan DevOps opdracht 4!</p>
         <a href="/blog"><button type="button" class="btn btn-secondary btn-lg my-5">Naar blog</button></a>
     </div>
 @endsection
